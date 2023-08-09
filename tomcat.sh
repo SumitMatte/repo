84d64a398e25ad0a1dd2cf51e4c753f2d36f07c5
Sumit B Matte <43988210+SumitMatte@users.noreply.github.com>
2023-08-10 00:35:48 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -4,7 +4,7 @@ sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
 sudo mkdir /opt/tomcat
 cd /opt/tomcat
 sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.78/bin/apache-tomcat-9.0.78.tar.gz
-cd
+cd /opt/tomcat
 sudo tar -xf apache-tomcat-*.tar.gz -C /opt/tomcat --strip-components=1
 sudo chown -R tomcat: /opt/tomcat
 echo 'export CATALINA_HOME="/opt/tomcat"' >> ~/.bashrc
