59d29a227487308128da66478a29846f836addde
Sumit B Matte <43988210+SumitMatte@users.noreply.github.com>
2023-08-10 00:06:45 +0530

    Update ec2.sh
    add tomcat9

--- ec2.sh
+++ ec2.sh
@@ -1,10 +1,10 @@
 sudo apt update
 
-sudo apt install git -y
+#sudo apt install git -y
 
 sudo apt install apache2 -y
 
 sudo apt install default-jdk -y
 
-sudo apt install tomcat -y
+sudo apt install tomcat9 -y
 
