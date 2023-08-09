ede1f74ea29dfdeb6325c766c28019d74a606e8a
Sumit B Matte <43988210+SumitMatte@users.noreply.github.com>
2023-08-10 00:22:42 +0530

    Create tomcat.sh

---
+++ tomcat.sh
@@ -0,0 +1,14 @@
+sudo groupadd tomcat
+sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
+
+sudo mkdir /opt/tomcat
+cd /opt/tomcat
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.78/bin/apache-tomcat-9.0.78.tar.gz
+
+sudo tar -xf apache-tomcat-*.tar.gz -C /opt/tomcat --strip-components=1
+sudo chown -R tomcat: /opt/tomcat
+echo 'export CATALINA_HOME="/opt/tomcat"' >> ~/.bashrc
+echo 'export JAVA_HOME="/usr/lib/jvm/default-java"' >> ~/.bashrc
+source ~/.bashrc
+$CATALINA_HOME/bin/startup.sh
+
