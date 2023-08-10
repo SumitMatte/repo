4834559f916b79ae86928320bc717352072d6c30
Sumit B Matte <43988210+SumitMatte@users.noreply.github.com>
2023-08-10 11:09:51 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -7,9 +7,17 @@ sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.78/bin/apache-tomcat-9.0
 cd /opt/tomcat
 sudo tar -xf apache-tomcat-*.tar.gz -C /opt/tomcat --strip-components=1
 sudo chown -R tomcat: /opt/tomcat
+sudo chown -R tomcat webapps/ work/ temp/ logs/
+
 echo 'export CATALINA_HOME="/opt/tomcat"' >> ~/.bashrc
-echo 'export JAVA_HOME="/usr/lib/jvm/default-java"' >> ~/.bashrc
+echo 'export JAVA_HOME="/usr/lib/jvm/java-1.11.0-openjdk-amd64"' >> ~/.bashrc
+
+echo 'export CATALINA_HOME="/opt/tomcat"' >> ~/.profile
+echo 'export JAVA_HOME="/usr/lib/jvm/java-1.11.0-openjdk-amd64"' >> ~/.profile
+
+
 source ~/.bashrc
 sudo /opt/tomcat/bin/catalina.sh start
 #$CATALINA_HOME/bin/startup.sh
+sudo ufw allow 8080
 
