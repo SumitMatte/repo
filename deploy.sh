4f9677a7951a39ee16b38e06b9df77ab295f2d41
Sumit B Matte <43988210+SumitMatte@users.noreply.github.com>
2023-08-10 11:30:57 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt install maven -y
+cd /mnt/
+git clone https://github.com/SumitMatte/onlinebookstore.git
+cd /mnt/onlinebookstore/
+mvn install
+sudo chmod -R o+rwx /var/lib/tomcat9/webapps/
+
+sudo cp -r target/onlinebookstore.war /var/lib/tomcat9/webapps/
+
+
+
+
