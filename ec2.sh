acf36e2567848f6b5c328b3f84e644da2a72fb92
Sumit B Matte <43988210+SumitMatte@users.noreply.github.com>
2023-08-10 11:18:25 +0530

    Update ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -6,6 +6,9 @@ sudo apt install apache2 -y
 
 sudo apt install default-jdk -y
 
+sudo apt update
+sudo apt-get install tomcat9 -y
+
 #sudo add-apt-repository ppa:tomcat9/stable
 
 #sudo apt-get install tomcat9 -y
