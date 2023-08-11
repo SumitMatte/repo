e08120026fcff878deebe598e32e1e9d418d3a9c
Sumit B Matte <43988210+SumitMatte@users.noreply.github.com>
2023-08-11 10:58:00 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 sudo apt install maven -y
 cd /mnt/
-git clone https://github.com/SumitMatte/onlinebookstore.git
+sudo git clone https://github.com/SumitMatte/onlinebookstore.git
 cd /mnt/onlinebookstore/
 mvn install
 sudo chmod -R o+rwx /var/lib/tomcat9/webapps/
